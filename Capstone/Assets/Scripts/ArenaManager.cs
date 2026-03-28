ad743e3e25d18f00f498d9c958977d9b1478b5bb
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2026-03-28 17:41:22 -0400

    created basic level and added gate

---
+++ Capstone/Assets/Scripts/ArenaManager.cs
@@ -0,0 +1,40 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour
+{
+    [SerializeField] AudioManager audioManager;
+	GameObject doorTrigger;
+    GameObject door;
+    int numberOfPlayersinArena = 0;
+    int numberOfPlayerInLobby = 1;
+
+	void Start(){
+		if (!audioManager) audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
+		doorTrigger = GameObject.Find("DoorTrigger");
+        var triggerProxy = doorTrigger.GetComponent<TriggerProxy>();//.OnTrigger += CloseDoor;
+        triggerProxy.OnTrigger += CloseDoor;
+        triggerProxy.tagName = "Player";
+
+        door = GameObject.Find("White_Gate");
+        
+    }
+
+    void Update(){
+        
+    }
+
+    void CloseDoor() {
+        numberOfPlayersinArena++;
+        numberOfPlayerInLobby = NetworkManager.Singleton == null ? 1 : 2;
+		if (numberOfPlayersinArena >= numberOfPlayerInLobby) {
+            // close door
+            door.GetComponent<Animator>().SetTrigger("CloseNow");
+            door.GetComponent<BoxCollider2D>().enabled = true;
+            audioManager.SFXVolume(100f);
+			audioManager.PlaySFX("GateClose");
+		}
+
+	}
+}
